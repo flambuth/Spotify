14a20ed295e5bf710a8906abce60cac62fe0aa8e
Fredrick Lambuth <flambuth@gmail.com>
2020-11-05 18:45:13 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -9,6 +9,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-inp = input("Enter an Artist ID")
+#inp = input("Enter an Artist ID")
 
-sys.stdout.write(', '.join(sp.artist(inp)['genres']))
+sys.stdout.write(', '.join(sp.artist(sys.argv[1])['genres']))
