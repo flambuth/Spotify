08dfab3b12879810e093dae3bc3b360287ca7926
Fredrick Lambuth <flambuth@gmail.com>
2020-12-13 23:27:13 -0600

    Daily Job to Database is complete
    I have a script (dail_job.py) that will insert into the spotify.db the 20 tracks for the day.

--- daily_job.py
+++ daily_job.py
@@ -13,7 +13,7 @@ import sqlite3
 
 #%% Create the daily 20 tracks dictionary
 
-daily_top_20 = get_user_top_tracks_artists()
+daily_top_20 = get_daily_top20_tracks()
 
 #%% Establish connection with db
 
@@ -22,32 +22,29 @@ try:
     cursor = sqliteConnection.cursor()
     sqlite_Query = "select * from daily_top20_tracks;"
     cursor.execute(sqlite_Query)
-    print(cursor.fetchall())
 
     
 except sqlite3.Error as error:
     print("Error while connecting to sqlite", error)
     
     
-#%% Insert the daily_top_20 in to the database
+#%% Using the dictionary method
 
-generator = daily_top_20.iterrows()
-
-sql_query = '''
+product_sql = '''
 INSERT INTO daily_top20_tracks (position, art_id, art_name, album_name, song_id, song_name, popularity, date) 
 VALUES (?, ?, ?, ?, ?, ?, ?, ?) '''
-
-for i in list(range(daily_top_20.shape[0])):
-    # current_song = next(generator)
-    # position = next(lard)[1]['daily_position']
-    # art_id = next(lard)[1]['art_id']
-    # art_name = next(lard)[1]['art_name']
-    # album_name = next(lard)[1]['album_name']
-    # song_id = next(lard)[1]['song_id']
-    # song_name = next(lard)[1]['song_name']
-    # popularity =next(lard)[1]['popularity']
-    # date = next(lard)[1]['date']
     
-    cursor.execute(sql_query, next(generator)[1].values)
+for i in range(len(daily_top_20['date'])):
+    track = [(daily_top_20['position'][i]),
+    (daily_top_20['art_id'][i]),
+    (daily_top_20['art_name'][i]),
+    (daily_top_20['album_name'][i]),
+    (daily_top_20['song_id'][i]),
+    (daily_top_20['song_name'][i]),
+    (daily_top_20['popularity'][i]),
+    (daily_top_20['date'][i])]
+    cursor.execute(product_sql, track)
 
-
+    
+sqliteConnection.commit()
+cursor.close()
