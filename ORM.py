1efc436563d9e9df5551881b6fcd4fbf2bcc7674
Fredrick Lambuth <flambuth@gmail.com>
2021-01-04 21:32:04 -0600

    SQLAlchemy mappings for the two tables
    It gets confusing after that

--- ORM.py
+++ ORM.py
@@ -1,1 +1,44 @@
 # -*- coding: utf-8 -*-
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+
+Base = declarative_base()
+
+daily_top20_tracks= Table(
+    "daily_top20_tracks",
+    Base.metadata,
+    ID = Column(Integer, primary_key=True)
+    position = Column(Integer, primary_key=True)
+    art_id = Column(Integer),
+    art_name = Column(String(250)),
+    album_name = Column(String(250)),
+    song_id = Column(String(250)),
+    song_name = Column(String(250)),
+    popularity = Column(Integer),
+    date  = Column(String(250))
+)
+    
+artists= Table(
+    "artits",
+    Base.metadata,
+    ID = Column(Integer)
+    art_id = Column(Integer, primary_key=True),
+    art_name = Column(String(250)),
+    song_name = Column(String(250)),
+    popularity = Column(Integer),
+    date  = Column(String(250))
+)
+    
+'''
+CREATE TABLE artists (
+                                    id INTEGER PRIMARY KEY,
+                                    art_id TEXT NOT NULL,
+                                    art_name TEXT NOT NULL,
+                                    followers INTEGER NOT NULL,
+                                    genre STRING NOT NULL,
+                                    popularity TEXT NOT NULL,
+                                    album_count INTEGER NOT NULL,
+                                    first_release TEXT NOT NULL,
+                                    query_date TEXT NOT NULL);'''
