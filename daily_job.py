e0e3d790482b7cef568ba6d3235a1af2ce82cc28
Fredrick Lambuth <flambuth@gmail.com>
2021-02-11 21:34:15 -0600

    garbage is good badn
    I'm a little drunk

--- daily_job.py
+++ daily_job.py
@@ -17,34 +17,6 @@ import sqlite3
 
 daily_top_20 = get_daily_top20_tracks()
 daily_top_artists = get_top_artists()
-
-# product_sql = '''
-# INSERT INTO daily_top20_tracks (position, art_id, art_name, album_name, song_id, song_name, popularity, date) 
-# VALUES (?, ?, ?, ?, ?, ?, ?, ?) '''
-
-#%% Write to the Database the day's top 20 tracks
-
-# try:
-#     sqliteConnection = sqlite3.connect('spotify.db')
-#     cursor = sqliteConnection.cursor()
-#     sqlite_Query = "select * from daily_top20_tracks;"
-#     cursor.execute(sqlite_Query)
-#     for i in range(len(daily_top_20['date'])):
-#         track = [(daily_top_20['position'][i]),
-#         (daily_top_20['art_id'][i]),
-#         (daily_top_20['art_name'][i]),
-#         (daily_top_20['album_name'][i]),
-#         (daily_top_20['song_id'][i]),
-#         (daily_top_20['song_name'][i]),
-#         (daily_top_20['popularity'][i]),
-#         (daily_top_20['date'][i])]
-#         cursor.execute(product_sql, track)
-    
-# except sqlite3.Error as error:
-#     print("Error while connecting to sqlite", error)
-
-# sqliteConnection.commit()
-# cursor.close()
     
 #%%    
 
