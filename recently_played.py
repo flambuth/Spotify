72c7857b9f6a93827044ceddc27c658008d574ad
Fredrick Lambuth <flambuth@gmail.com>
2021-08-17 15:44:00 -0500

    Update recently_played.py
    i put it in fredlambuth.co. it works

--- recently_played.py
+++ recently_played.py
@@ -5,13 +5,14 @@ from spotipy.oauth2 import SpotifyOAuth
 from config import username
 
 #scope is narrowed just to what the 'user/me' has recently played.
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(username=username, scope='user-read-recently-played'))
+def last_song_played():
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(username=username, scope='user-read-recently-played'))
 
-spotify_request = sp.current_user_recently_played(limit=1)
+    spotify_request = sp.current_user_recently_played(limit=1)
+    last_song = spotify_request['items'][0]
+    artist_name = last_song['track']['artists'][0]['name']
+    song_name =  last_song['track']['name']
+    song_link = last_song['track']['external_urls']['spotify']
+    last_played = last_song['played_at']
 
-last_song = spotify_request['items'][0]['track']
-
-artist_name = last_song['track']['artists'][0]['name']
-song_name =  last_song['track']['name']
-song_link = last_song['track']['external_urls']['spotify']
-last_played = spotify_request['played_at']
+    return dict(art_name=artist_name, song_name=song_name, song_link=song_link, last_played=last_played)
